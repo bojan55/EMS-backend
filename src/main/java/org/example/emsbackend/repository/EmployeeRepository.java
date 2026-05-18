c314ab3b97ecaaf2d1047dfffcd6dd47bcf547df
bojan55 <bojan.bajic55@gmail.com>
2026-05-18 17:27:41 +0200

    Create EmployeeRepository interface.

---
+++ src/main/java/org/example/emsbackend/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package org.example.emsbackend.repository;
+
+import org.example.emsbackend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
